c217f3302ea13ad09ac874e7eb5ae21a9546881d
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-03-15 18:51:28 +0300

    add: add chevronClickHandler

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
   console.log('Hello Rolling Scopes!');
   // NavbsarClick
   addNavbarClickHandler();
+  addChevronClickHandlers();
 
   
 }
@@ -17,3 +18,60 @@ const addNavbarClickHandler = () => {
   });
 }
 
+// chev chev-left
+
+const changeBachgroundColor = (index) => {
+  const slider = document.querySelector('.slider')
+
+  if (index % 2 === 0) {
+    slider.classList.add('color-red')
+    slider.classList.remove('color-blue')
+  } else {
+    slider.classList.add('color-blue')
+    slider.classList.remove('color-red')
+  }
+
+  
+
+  
+}
+
+const addChevronClickHandlers = () => {
+
+
+  let selectedSlideIndex = 0
+  const allSlides = Array.from(document.querySelectorAll('.slide'))
+  const slideCount = allSlides.length
+
+
+  document.querySelector('.chev.chev-right').addEventListener('click', event => {
+
+    if (selectedSlideIndex < slideCount - 1) {
+      selectedSlideIndex++
+    } else {
+      selectedSlideIndex = 0
+    }
+
+    document.querySelectorAll('.slide').forEach(el => el.classList.remove('active'))
+    allSlides[selectedSlideIndex].classList.add('active') 
+    
+    changeBachgroundColor(selectedSlideIndex)
+
+  })
+
+
+  document.querySelector('.chev.chev-left').addEventListener('click', event => {
+    
+    if (selectedSlideIndex > 0) {
+      selectedSlideIndex--
+    } else {
+      selectedSlideIndex = slideCount - 1
+    }
+
+    document.querySelectorAll('.slide').forEach(el => el.classList.remove('active'))
+    allSlides[selectedSlideIndex].classList.add('active') 
+   
+    changeBachgroundColor(selectedSlideIndex)
+
+  })
+}
